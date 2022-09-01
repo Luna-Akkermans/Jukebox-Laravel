caea2bed8a64251dc683d7cf6d5fcbb4daffc39f
LMA <99044668@mydavinci.nl>
2022-09-01 17:04:18 +0200

    added frontpage + logout logic

--- routes/web.php
+++ routes/web.php
@@ -32,5 +32,6 @@ Route::get('/register', function () {
 
 
 Auth::routes();
+Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
